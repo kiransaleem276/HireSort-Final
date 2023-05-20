c5282a5b1c1199704438b7e5c3a6cba4db984606
Kiran Muhamamd Saleem <kiran.saleem@paymytuition.com>
2023-05-20 12:13:41 +0500

    Add project files.

---
+++ HireSort Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HireSort_Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
