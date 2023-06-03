cbb76aabaa6dcb0d3d14f30f9282361cf59c16cf
Kiran Muhamamd Saleem <kiran.saleem@paymytuition.com>
2023-06-03 19:26:31 +0500

    about us and contact us

---
+++ HireSort Final/Controllers/AboutUsController.cs
@@ -0,0 +1,31 @@
+﻿using HireSort.Models;
+using HireSort.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace HireSort.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly ILogger<AboutUsController> _logger;
+        private IDashboard _dashboard;
+
+        public AboutUsController(ILogger<AboutUsController> logger, IDashboard dashboard)
+        {
+            _logger = logger;
+            _dashboard = dashboard;
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HireSort Final/Controllers/ContactUsController.cs
@@ -0,0 +1,32 @@
+﻿using HireSort.Models;
+using HireSort.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace HireSort.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly ILogger<ContactUsController> _logger;
+        private IDashboard _dashboard;
+
+        public ContactUsController(ILogger<ContactUsController> logger, IDashboard dashboard)
+        {
+            _logger = logger;
+            _dashboard = dashboard;
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
